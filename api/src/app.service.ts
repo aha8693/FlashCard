988b2765073e4aa51f0c051c86710029846a0056
Anna <hyeanan@gmail.com>
2023-12-02 21:08:30 -0500

    no console.log

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {}
-
   getHello(): string {
-    const NODE_ENV = this.configService.get<string>("NODE_ENV");
-    console.log(NODE_ENV);
     return "Hello World!";
   }
 }
